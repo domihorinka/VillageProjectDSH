44da986e595d4ac5d0682a01183ac251a81eef5d
domihorinka <domihorinka@gmail.com>
2021-09-12 16:03:07 -0600

    added styling to navbar and buttons

--- village-activities/src/components/Navbar.js
+++ village-activities/src/components/Navbar.js
@@ -1,13 +1,29 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from './Button';
+import './Navbar.css';
 
 
 function Navbar() {
     const [click, setClick] = useState(false);
-    
+    const [button, setButton] =useState(true);
+
     const handleClick = () => setClick(!click);
     const closeMobileMenu =() => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false);
+        }
+        else {
+            setButton(true);
+        }
+    };
+
+    window.addEventListener('resize', showButton);
+
     return (
+
         <>
          <nav className='navbar'>
             <div className='navbar-container'>
@@ -33,9 +49,7 @@ function Navbar() {
                             About
                         </Link>
                         <li className='nav-item'>
-                        <Link to='/donate' className='nav-links' onClick={closeMobileMenu}>
-                            Donate
-                        </Link>
+                
                         <li className='nav-item'>
                         <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
                             Sign Up
@@ -44,6 +58,7 @@ function Navbar() {
                     </li>
                     </li>
                 </ul>
+                {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
              </div>
          </nav>
         </>
